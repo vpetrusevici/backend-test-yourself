4ba95b7d172aac75928782e38ab778295ad6dd6c
Vladimir Petrusevici <vladimir.petrusevici@amdaris.com>
2022-08-15 20:23:16 +0300

    Implimintation

---
+++ backend-test-yourself/ListExtenstions.cs
@@ -0,0 +1,33 @@
+﻿
+namespace backend_test_yourself;
+public static class IEnumberableExtenstions
+{
+    public static IEnumerable<G> Map<T, G>(this IEnumerable<T> source, Func<T, G> func)
+    {
+        var newList = new List<G>();
+        foreach (var item in source)
+        {
+            newList.Add(func(item));
+        }
+        return newList;
+    }
+
+    public static G Fold<T, G>(this IEnumerable<T> source, G initialState, Func<G, T, G> func)
+    {
+        var currentState = initialState;
+        foreach (var item in source)
+        {
+            currentState = func(currentState, item);
+        }
+        return currentState;
+    }
+
+    public static IEnumerable<G> Map2<T, G>(this IEnumerable<T> source, Func<T, G> func)
+        => source.Fold(
+            new List<G>(),
+            (accum, item) =>
+            {
+                accum.Add(func(item));
+                return accum;
+            });
+}
---
+++ backend-test-yourself/Program.cs
@@ -0,0 +1,18 @@
+﻿using backend_test_yourself;
+using Newtonsoft.Json;
+
+var exampleValues = new[] { 1, 2, 3, 4 };
+
+var result1 = exampleValues.Map(i => i.ToString());
+
+Console.WriteLine($"Map result: {JsonConvert.SerializeObject(result1)}");
+
+
+var result2 = exampleValues.Fold("", (str, x) => str + x);
+
+Console.WriteLine($"Fols result: {JsonConvert.SerializeObject(result2)}");
+
+
+var result3 = exampleValues.Map2(i => i.ToString());
+
+Console.WriteLine($"Map2 result: {JsonConvert.SerializeObject(result3)}");
